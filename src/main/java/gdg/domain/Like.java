ddc1eb1350d374065600f6aba76108bd8c80db9b
황선아 <tjsdk2769@gmail.com>
2021-07-11 04:03:15 +0900

    Add Like

--- src/main/java/gdg/domain/Like.java
+++ src/main/java/gdg/domain/Like.java
@@ -1,4 +1,19 @@
-package gdg.domain;
-
-public class Like extends BaseTimeEntity{
-}
+//package gdg.domain;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
+//
+//@Getter
+//@Entity
+//@Table(name = "LIKE")
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+//public class Like{
+//
+//}
